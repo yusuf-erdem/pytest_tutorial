7098f657ea5c85fb74c105d975a7a4c189b90512
Yusuf Erdem <yusuf@thundra.io>
2022-06-21 09:58:32 +0300

    multi coverage report merge feature

---
+++ tests2/test_sum.py
@@ -0,0 +1,6 @@
+from rtanalysis.rtanalysis import RTAnalysis
+
+
+def test_rtanalysis_fit():
+    rta = RTAnalysis()
+    rta.sum()
---
+++ tests2/test_sum_demo.py
@@ -0,0 +1,6 @@
+from coverage_demo.rtanalysis import RTAnalysis
+
+
+def test_rtanalysis_fit():
+    rta = RTAnalysis()
+    rta.add()
